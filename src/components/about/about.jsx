20117ff64f2be8ae8979263943f6fac6296f3a4d
archit1599 <architbambhorikar15@gmail.com>
2021-12-04 18:29:04 +0530

    first commit

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -17,7 +17,7 @@ function about() {
                 Paragraphs Demo
                 </p>
                 <p className="a-sub">
-                The Paragraphs module in Drupal provides editors with a component driven architecture for building pages. Morpht has been developing Paragraph approaches to site building since 2015. This case study captures some of the content from a Drupal 7 "Paragraphs Demo" site which was built to showcase what could be done with Paragraphs.
+                The Paragraphs module in Drupal provides editors with a component driven architecture for building pages. Morpht has been developing Paragraph approaches to site building since 2015. This case study captures some of the content from a Drupal 7 "Paragraphs Demo".
                 </p>
             </div>
         </div>
